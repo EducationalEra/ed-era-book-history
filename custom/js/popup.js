0276bffcaf8d75624cc3b09af70029ea3e26a3fd
artemilchuk <artem.ilchuk@ed-era.com>
2015-09-09 14:09:17 +0300

    Update custom/js/popup.js

--- custom/js/popup.js
+++ custom/js/popup.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+document.getElementByTag("div.fluidMedia").addEventListener("click", console.log("123123"));
+/*$(document).ready(function(){
     $( "div.popup" ).replaceWith( "<div class='" + "description" + "'><div class='" + "text" +"'>Ще більше відеоматеріалів в <a class='"+"knopka1"+"' href='" + "https://study.ed-era.com/courses/EdEra/hi102/Hi102/about" + "' target='" + "_blank" + "'>онлайн-курсі</a> від розробників цієї книги</div><div class='" + "btn-knopka" +"'><a class='"+"knopka"+"' href='" + "https://study.ed-era.com/courses/EdEra/hi102/Hi102/about" + "'   target='" + "_blank" + "'>Перейти на курс</a></div>" );
-    });
+    });*/
 
